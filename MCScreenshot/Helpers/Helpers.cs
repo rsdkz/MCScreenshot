7c63b5d5e189f6570ec38bc3c2acd5166e6064d9
rsdkz <dk125532@gmail.com>
2017-02-04 19:39:33 +0100

    Working on new Helper things

--- MCScreenshot/Helpers/Helpers.cs
+++ MCScreenshot/Helpers/Helpers.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Drawing;
 
 namespace MCScreenshot.Helpers
 {
     /* currently unused */
     public class Helpers
     {
+
+        /* Begin of Pinvoke and Stuff */
         [DllImport("user32.dll", SetLastError = true)]
         private static extern unsafe IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern unsafe bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        private static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        /* End of Pinvoke and Stuff */
 
         public static IntPtr FindWindowByTitle(string windowTitle)
         {
@@ -22,6 +41,12 @@ namespace MCScreenshot.Helpers
                 return hWnd;
             }
             else { return new IntPtr(-1); }
+        }
+
+        public static Rectangle GetClientRectangle(IntPtr hWnd)
+        {
+            // wip
+            return new Rectangle();
         }
     }
 }
